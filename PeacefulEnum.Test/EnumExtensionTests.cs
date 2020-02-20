9d19c334db58cdb6c459125aec9088ee891c379f
AJ Jadoon <ajjadoon18@gmail.com>
2020-02-20 15:22:54 +0100

    Refactor methods and add new Description method

--- PeacefulEnum.Test/EnumExtensionTests.cs
+++ PeacefulEnum.Test/EnumExtensionTests.cs
@@ -6,51 +6,72 @@ namespace PeacefulEnum.Test
     public sealed class EnumExtensionTests
     {
         [Fact]
-        public void IsEnumValidShouldReturnFalseWhenValueIsOutOfRange()
+        public void IsValidShouldReturnFalseWhenValueIsOutOfRange()
         {
             Mood mood = (Mood)300;
-            Assert.False(mood.IsEnumValid());
+            Assert.False(mood.IsValid());
         }
 
         [Fact]
-        public void IsEnumValidShouldReturnTrueWhenValueIsInRange()
+        public void IsValidShouldReturnTrueWhenValueIsInRange()
         {
             Mood mood = (Mood)1;
-            Assert.True(mood.IsEnumValid());
+            Assert.True(mood.IsValid());
         }
 
         [Fact]
-        public void IsEnumValidShouldReturnFalseWhenValueIsNull()
+        public void IsValidShouldReturnFalseWhenValueIsNull()
         {
             Mood? mood = null;
-            Assert.False(mood.IsEnumValid());
+            Assert.False(mood.IsValid());
         }
 
         [Fact]
-        public void IsEnumNullOrDefaultShouldReturnTrueWhenValueIsNull()
+        public void IsNullOrDefaultShouldReturnTrueWhenValueIsNull()
         {
             Mood? mood = null;
-            Assert.True(mood.IsEnumNullOrDefault());
+            Assert.True(mood.IsNullOrDefault());
         }
 
         [Fact]
-        public void IsEnumNullOrDefaultShouldReturnTrueWhenValueIsDefault()
+        public void IsNullOrDefaultShouldReturnTrueWhenValueIsDefault()
         {
             Mood mood = Mood.Bored;
-            Assert.True(mood.IsEnumNullOrDefault((int)Mood.Bored));
+            Assert.True(mood.IsNullOrDefault((int)Mood.Bored));
         }
 
         [Fact]
-        public void IsEnumNullOrDefaultShouldReturnFalseWhenValueIsNotDefault()
+        public void IsNullOrDefaultShouldReturnFalseWhenValueIsNotDefault()
         {
             Mood mood = Mood.Peaceful;
-            Assert.False(mood.IsEnumNullOrDefault());
+            Assert.False(mood.IsNullOrDefault());
+        }
+
+        [Fact]
+        public void WhenEnumIsnullDescriptionShouldBeEmpty()
+        {
+            Mood? mood = null ;
+            Assert.Equal(string.Empty, mood.Description());
+        }
+
+        [Fact]
+        public void WhenEnumHasDescriptionTagThenDescriptionShouldBeReturned()
+        {
+            Mood mood = Mood.Peaceful;
+            Assert.Equal("Description of peaceful", mood.Description());
+        }
+
+        [Fact]
+        public void WhenEnumHasNoDescriptionTagThenDescriptionReturnsEmptyString()
+        {
+            Mood mood = Mood.Happy;
+            Assert.Equal(string.Empty, mood.Description());
         }
 
         private enum Mood
         {
             Undefined = 0,
-            [Description("Description of peacefull")]
+            [Description("Description of peaceful")]
             Peaceful = 1,
             Angry = 2,
             Happy = 3,
